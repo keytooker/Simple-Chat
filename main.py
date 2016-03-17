a28c358551c8d60e976742267cdf1fba8a4e27ae
Mikhail Visloguzov <keytooker@gmail.com>
2016-03-17 15:59:42 +0300

    Изменил main.py чтобы запускал main.html

--- main.py
+++ main.py
@@ -7,8 +7,20 @@ import tornado.escape
 import tornado.options
 
 class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello Heroku from mivi!")
+    def get(self, q):
+        if 'GOOGLEANALYTICSID' in os.environ:
+            google_analytics_id = os.environ['GOOGLEANALYTICSID']
+        else:
+            google_analytics_id = False
+
+        self.render(
+            "main.html",
+            page_title='Heroku Funtimes',
+            page_heading='777!',
+            google_analytics_id=google_analytics_id,
+        )
+    #def get(self):
+        #self.write("Hello Heroku from mivi!")
 
 # application settings and handle mapping info
 class Application(tornado.web.Application):
