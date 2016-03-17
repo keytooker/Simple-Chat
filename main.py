906fd9aeedc51005483174bba82885b45de7c720
Mikhail Visloguzov <keytooker@gmail.com>
2016-03-17 16:40:02 +0300

    Добавил каталог static. http_server запускаю через наследованный от Application class

--- main.py
+++ main.py
@@ -6,6 +6,19 @@ import tornado.web
 import tornado.escape
 import tornado.options
 
+# application settings and handle mapping info
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler)
+        ]
+        settings = dict(
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            debug=True,
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+        
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello Heroku from mivi!")
@@ -13,10 +26,10 @@ class MainHandler(tornado.web.RequestHandler):
         self.render("main.html", title="Super title", items=items)
         
 def main():
-    application = tornado.web.Application([
-        (r"/", MainHandler),
-    ])
-    http_server = tornado.httpserver.HTTPServer(application)
+#    application = tornado.web.Application([
+#        (r"/", MainHandler),
+#    ])
+    http_server = tornado.httpserver.HTTPServer(Application())
     port = int(os.environ.get("PORT", 5000))
     http_server.listen(port)
     tornado.ioloop.IOLoop.instance().start()
