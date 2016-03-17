4d123a9e233f18c30b58a4212f52b07e23bd6f6d
Mikhail Visloguzov <keytooker@gmail.com>
2016-03-17 15:53:54 +0300

    I added class Application

--- main.py
+++ main.py
@@ -6,20 +6,29 @@ import tornado.web
 import tornado.escape
 import tornado.options
 
-# import and define tornado-y things
-from tornado.options import define
-define("PORT", default=5000, help="run on the given port", type=int)
-
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello Heroku from me!")
- 
+        self.write("Hello Heroku from mivi!")
+
+# application settings and handle mapping info
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/([^/]+)?", MainHandler)
+        ]
+        settings = dict(
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            debug=True,
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
 def main():
     application = tornado.web.Application([
         (r"/", MainHandler),
     ])
     http_server = tornado.httpserver.HTTPServer(application)
-    # port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     http_server.listen(port)
     tornado.ioloop.IOLoop.instance().start()
  
