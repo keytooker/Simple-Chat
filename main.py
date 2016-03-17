098133a9151895d15eaa8f3d6d3efead7f220b48
Mikhail Visloguzov <keytooker@gmail.com>
2016-03-17 15:49:48 +0300

    Сменил определение for port

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import tornado.web
 import tornado.escape
 import tornado.options
 
+# import and define tornado-y things
+from tornado.options import define
+define("PORT", default=5000, help="run on the given port", type=int)
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello Heroku from me!")
@@ -15,7 +19,7 @@ def main():
         (r"/", MainHandler),
     ])
     http_server = tornado.httpserver.HTTPServer(application)
-    port = int(os.environ.get("PORT", 5000))
+    # port = int(os.environ.get("PORT", 5000))
     http_server.listen(port)
     tornado.ioloop.IOLoop.instance().start()
  
